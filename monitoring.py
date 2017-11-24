8c2f19acfeb65c4dda9ff6b3a0d1323766577d02
Azalee Bostroem <abostroem@gmail.com>
2017-11-24 09:44:50 -0800

    bug fixing

--- monitoring.py
+++ monitoring.py
@@ -7,10 +7,10 @@ for ifile in flist:
     with open(ifile, 'r') as ofile:
         all_lines = ofile.readlines()
         with open(ifile.replace('Data/lum_observed.dat', 'parameters'), 'r') as  param_file:
-            all_lines = param_file.readlines()
-            for iline in all_lines:
+            all_lines_param = param_file.readlines()
+            for iline in all_lines_param:
                 if iline.startswith('tend'):
                     tend = float(iline.split('=')[1].replace('d', 'e').strip('\n'))
                     break
-            print('{:2.2f} for {}'.format(float(all_lines[-1].split()[0])/tend, ifile))
+        print('{:2.2f} for {}'.format(float(all_lines[-1].split()[0])/tend, ifile))
 
