7e1d3b953cabd10fd04eec48e98e0aa0cbc2ae8e
Azalee Bostroem <abostroem@gmail.com>
2017-11-24 09:41:48 -0800

    fixing bugs in monitoring script

--- monitoring.py
+++ monitoring.py
@@ -4,13 +4,12 @@ import glob
 flist = sorted(glob.glob('snec_models/*/*/*/*/*/*/Data/lum_observed.dat'))
 
 for ifile in flist:
-    ofile = open(ifile, 'r')
-    all_lines = ofile.readlines()
-    ofile.close()
-    param_file = open(ifile.replace('Data/lum_observed.dat', 'parameters'))
-    all_lines = ofile.readlines()
-    for iline in all_lines:
-        if iline.startswith('tend'):
-            tend = float(iline.split('=')[1])
-    print('{:2.2f} for {}'.format(float(all_lines[-1].split()[0])/tend, ifile))
+    with open(ifile, 'r') as ofile:
+        all_lines = ofile.readlines()
+        with open(ifile.replace('Data/lum_observed.dat', 'parameters'), 'r') as  param_file:
+            all_lines = param_file.readlines()
+            for iline in all_lines:
+                if iline.startswith('tend'):
+                    tend = float(iline.split('=')[1])
+            print('{:2.2f} for {}'.format(float(all_lines[-1].split()[0])/tend, ifile))
 
