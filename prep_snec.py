3b3386b808a3c4b7a04b9a49d92fbd4905be7753
Azalee Bostroem <abostroem@gmail.com>
2017-11-24 08:58:46 -0800

    modifying to call a single file in array jobs rather than creating one file per array

--- prep_snec.py
+++ prep_snec.py
@@ -67,22 +67,21 @@ def copy_files(basepath, imass, idensity, iradius):
     shutil.copyfile('snec', os.path.join(basepath, 'snec'))
     shutil.copymode('snec', os.path.join(basepath, 'snec'))
 
-def write_sbatch_executable(basepath, array_num, snec_dir):
+def write_directory_list(path_list):
     '''
     Create a file to execute snec for each array job
     '''
-    ofile = open('snec{}.sh'.format(array_num), 'w')
-    ofile.write('#!/bin/bash -l \n')
-    ofile.write('cd {}\n'.format(os.path.join(snec_dir, basepath)))
-    ofile.write('./snec &>snec.out\n')
+    ofile = open('input_dir_list.txt'.format(array_num), 'w')
+    for ipath in path_list:
+        ofile.write('{}\n'.format(ipath))
     ofile.close()
-    os.chmod('snec{}.sh'.format(array_num), stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH | stat.S_IWUSR | stat.S_IRUSR)
+    os.chmod('input_dir_list.txt'.format(array_num), stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH | stat.S_IWUSR | stat.S_IXUSR)
 
-def write_sbatch_job(timeout):
+def write_sbatch_job(timeout, array_num):
     '''
     Creates script to be called by sbatch
     '''
-    ofile = open('snec_master.sh', 'w')
+    ofile = open('snec_array_job.sh', 'w')
     ofile.write(
     '''#! /bin/bash -l 
 #SBATCH -t {}:00:00
@@ -90,15 +89,20 @@ def write_sbatch_job(timeout):
 #SBATCH --mail-type=end 
 #SBATCH --mail-user=kabostroem@ucdavis.edu 
 #SBATCH --job-name='M={}-{},E={}-{},K={}-{}, R={}-{}'
-srun ./snec${}SLURM_ARRAY_TASK_ID{}.sh 
+#SBATCH -o name_%A_%a.out # Standard output
+#SBATCH -e name_%A_%a.err # Standard error
+#SBATH --array=1-{}
+export SEEDFILE=input_dir_list.txt
+export SEED=$(cat $SEEDFILE | head -n $SLURM_ARRAY_TASK_ID | tail -n 1)
+cd $SEED
+./snec &>snec.out
    '''.format(timeout, parameters['mass'][0], parameters['mass'][-1],
                                             parameters['explosion_energy'][0], parameters['explosion_energy'][-1],
                                             parameters['density_1D'][0], parameters['density_1D'][-1],
                                             parameters['wind_extent'][0], parameters['wind_extent'][-1],
-                                            '{', '}')
-        )
+                                            array_num)        )
     ofile.close()
-    os.chmod('snec_master.sh', stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH | stat.S_IWUSR | stat.S_IRUSR)
+    os.chmod('snec_array_job.sh', stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH | stat.S_IWUSR | stat.S_IRUSR)
     
     
 if __name__ == "__main__":      
@@ -111,6 +115,7 @@ if __name__ == "__main__":
     parameters = get_input_parameters()
     profile_manipulation.add_wind(parameters)
     array_num=0
+    path_list = []
     for indx_ni_mass, i_ni_mass in enumerate(parameters['ni_mass']):
         for indx_ni_mix, i_ni_mix in enumerate(parameters['ni_mix']):
             for indx_mass, imass in enumerate(parameters['mass']):
@@ -121,11 +126,11 @@ if __name__ == "__main__":
                             copy_files(basepath, imass, idensity, iradius)
                             parfile_organizer.write_parfile(i_ni_mass, i_ni_mix, imass, ienergy, parameters, basepath)
                             array_num +=1
-                            write_sbatch_executable(basepath, array_num, snec_dir)
-    
-    write_sbatch_job(args.timeout)
+                            path_list.append(os.path.join(snec_dir, basepath))
+    write_directory_list(path_list)
+    write_sbatch_job(args.timeout, array_num)
     print('Ready to run, start your jobs with the command:')
-    print('sbatch --partition high --array=1-{} ./snec_master.sh'.format(array_num))
+    print('sbatch --partition high ./snec_array_job.sh'.format(array_num))
                     
                         
 
