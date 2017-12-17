accb23b591f0ba66d30d97de0b645614724ce04c
Azalee Bostroem <abostroem@gmail.com>
2017-12-17 07:37:58 -1000

    changing - to _

--- run1_check.py
+++ run1_check.py
@@ -6,4 +6,4 @@ import prep_snec
 parameters = prep_snec.get_input_parameters()
 search_dir = '/home/bostroem/SNEC/snec_models'
 
-check_run.check-run(parameters, search_dir)
+check_run.check_run(parameters, search_dir)
