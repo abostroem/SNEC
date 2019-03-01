a2f71d8ee85333c05f4d40194cb068a7b47a8b9b
Azalee Bostroem <abostroem@gmail.com>
2019-03-01 13:39:35 -0800

    working on creating file if it doesn't exist - added a warning about not testing

--- copy_missing_dirs.py
+++ copy_missing_dirs.py
@@ -1,16 +1,35 @@
 import os
+import glob
 import shutil
 
-ofile = open('input_dir_list.txt', 'r')
-dir_list = ofile.readlines()
-line_list = [1113,1808,1829,1830,1831,1832,1837,1839,1842,2339,2340,4125,4174,4177,4179,
-             4180,4183,4194,4198,4213,4234,4245,4249,4266,4275,4277,4279,4289,4309,4316,
-             4317,4318,4319,4320,4321,4322,4323,4324,4325,4326,4327,4328,4329,4330,4331,
-             4690,4705,4879,4885,4888,4889,4911,4912,4913,4938,4939,4940,4942,4947,4948,
-             4956,4962]
-for iline in line_list:
-    copy_to_dir = dir_list[iline-1].strip('\n') #These are 1 indexed for slurm
-    copy_from_dir = copy_to_dir.replace('SNEC', 'SNEC2/SNEC')
-    shutil.copy2(copy_from_dir, copy_to_dir)
-    print('copied {} to {}'.format(copy_from_dir, copy_to_dir))
+base_dir = '/dark/SNEC/snec_models'
+from_dir = '/home/bostroem/SNEC_bare_15oz/SNEC/snec_models'
 
+input('THIS CODE ISNT TESTED - VERIFY THAT IT WORKS BEFORE RUNNING (enter to continue, ctl+c to exit ')
+for ini_mass in ni_masses:
+    new_path = os.path.join(base_dir, 'Ni_mass_{:1.4f}'.format(i_ni_mass))
+    if not os.path.exists(new_path):
+        os.mkdir(new_path)
+    for ini_mix in ni_mix:
+        new_path = os.path.join(new_path, 'Ni_mixing_{:1.1f}'.format(i_ni_mix))
+        if not os.path.exists(new_path):
+            os.mkdir(new_path)
+        for imass in masses:
+            new_path = os.path.join(new_path, 'M{:2.1f}'.format(imass))
+            if not os.path.exists(new_path):
+                os.mkdir(new_path)
+            for k in kvalues:
+                new_path = os.path.join(new_path, 'K_{:2.1f}'.format(idensity))
+                if not os.path.exists(new_path):
+                    os.mkdir(new_path)
+                for r in radii:
+                    new_path = os.path.join(new_path, 'R_{}'.format(int(iradius)))
+                    if not os.path.exists(new_path):
+                        os.mkdir(new_path)
+                    new_files = glob.glob(os.path.join(new_path.replace(base_dir, from_dir), '*.gz'))
+                    for ifile in new_files:
+                        if not os.path.exists(ifile.replace(from_dir, base_dir)):
+                            shutil.copyfile(ifile.replace(from_dir, base_dir))
+                        else:
+                            print('file already exists, not copying: {}'.format(ifile))
+                        
